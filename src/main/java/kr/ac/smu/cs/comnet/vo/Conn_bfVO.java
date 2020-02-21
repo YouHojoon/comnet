2dc6789e55accdb7dbaf9e4878041a485016ab83
YouHojoon <dbghwns66@daum.net>
2020-02-22 04:24:23 +0900

    fix:select board list

---
+++ src/main/java/kr/ac/smu/cs/comnet/vo/Conn_bfVO.java
@@ -0,0 +1,19 @@
+package kr.ac.smu.cs.comnet.vo;
+
+public class Conn_bfVO {
+	int bid;
+	int fid;
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public int getFid() {
+		return fid;
+	}
+	public void setFid(int fid) {
+		this.fid = fid;
+	}
+	
+}
---
+++ src/main/java/kr/ac/smu/cs/comnet/vo/Conn_blVO.java
@@ -0,0 +1,19 @@
+package kr.ac.smu.cs.comnet.vo;
+
+public class Conn_blVO {
+	int bid;
+	int lid;
+	public int getBid() {
+		return bid;
+	}
+	public void setBid(int bid) {
+		this.bid = bid;
+	}
+	public int getLid() {
+		return lid;
+	}
+	public void setLid(int lid) {
+		this.lid = lid;
+	}
+	
+}
